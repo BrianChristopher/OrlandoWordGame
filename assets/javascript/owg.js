ac518270e70b49815eb4e0cf1c55c6f3fb369ea8
Brian Christopher <bcwood79@yahoo.com>
2019-06-21 22:00:54 -0400

    User guess pushes to lettersGuessedArray.

--- assets/javascript/owg.js
+++ assets/javascript/owg.js
@@ -2,7 +2,8 @@
 
 var splitWord = [];
 var testWord = "Orange Blossom Trail";
-var lettersGuessed = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "a"]
+//The length of this array before any letters are pushed is 39
+var lettersGuessedArray = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "~", "`", "[", "]", "{", "}", "-", "_", "+", "=", "/", ",", ".", "/", "<", ">", "?", ";", ":", '"', "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var wordView = "";
 var currentBuildRow = 0;
 
@@ -37,7 +38,7 @@ var displayWord = function () {
 
 buildOranges(testWord);
 
-
+console.log(lettersGuessedArray.length)
 
 
 
@@ -45,11 +46,25 @@ buildOranges(testWord);
 
 
 //This gets the guess from the user
-$("#submitUserGuess").on("click", function(event){
+$("#submitUserGuess").on("click", function (event) {
     event.preventDefault();
     var letterGuessed = $("#userGuess").val().trim();
     $("#userGuess").val("");
     console.log(letterGuessed);
-    //topics.push(newTopic);
+
+    //if letter was already guessed, alert that a new guess is needed
+    if (lettersGuessedArray.includes(letterGuessed)) {
+        alert("You chose poorly. Guess again");
+    }
+
+    else {
+        var lowerGuess = letterGuessed.toLowerCase();
+        lettersGuessedArray.push(lowerGuess);
+        var upperGuess = letterGuessed.toUpperCase();
+        lettersGuessedArray.push(upperGuess);
+        console.log(lettersGuessedArray);
+    }
+
     
+
 });
