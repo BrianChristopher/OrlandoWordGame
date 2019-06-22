09260b11e4897ab949f8e9b3b1937bafb87b4b75
Brian Christopher <bcwood79@yahoo.com>
2019-06-21 20:59:47 -0400

    Orange function altered so that new words show up on new line/row.

--- assets/javascript/owg.js
+++ assets/javascript/owg.js
@@ -4,23 +4,37 @@ var splitWord = [];
 var testWord = "Orange Blossom Trail";
 var lettersGuessed = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "a"]
 var wordView = "";
+var currentBuildRow = 0;
 
 
+
+//This function turns the phrase into a string of characters and then displays the characters within the Orange Card slices.
 var buildOranges = function (wordToGuess) {
-
-    splitWord = testWord.split("");
+    splitWord = wordToGuess.split("");
     console.log(testWord);
     console.log(splitWord);
+    currentBuildRow = 1;
+
     for (i = 0; i < splitWord.length; i++) {
         var nextCharacter = splitWord[i];
 
-        var frontOrange = $("<div>").addClass("orangeCardFront");
-        var backOrange = $("<div>").addClass("orangeCardBack").text(nextCharacter);
-        var wholeOrange=frontOrange.append(backOrange);
-        var orangeSlice= $("<div>").attr({class: "orangeCard", id: ("oc" + i)}).html(wholeOrange)
-        $("#buildWordHere").append(orangeSlice);
+        if (nextCharacter == " ") {
+            //var newLine = $("<br>");
+            currentBuildRow++;
+            console.log(currentBuildRow);
+        }
+        else {
+            var frontOrange = $("<div>").addClass("orangeCardFront");
+            var backOrange = $("<div>").addClass("orangeCardBack").text(nextCharacter);
+            var wholeOrange = frontOrange.append(backOrange);
+            var orangeSlice = $("<div>").attr({ class: "orangeCard", id: ("oc" + i) }).html(wholeOrange)
+            $("#buildWordHere" + currentBuildRow).append(orangeSlice);
+        }
     }
+}
 
+var displayWord = function () {
+    buildOranges(testWord);
 }
 
 buildOranges(testWord);
